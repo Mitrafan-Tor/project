6536ca8477b2e4d60d525b618ef895e4a95a38d2
Mitrafan-Tor <dmitrytorzhikov@gmail.com>
2025-04-14 16:02:16 +0400

    кеширование

--- project/board/views.py
+++ project/board/views.py
@@ -2,11 +2,11 @@
 from django.http import HttpResponse
 from django.views import View
 from project.tasks import hello, printer
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 class IndexView(View):
     def get(self, request):
-        printer.apply_async([10],eta = datetime.now() + timedelta(seconds=5))
+        printer.apply_async([10],eta = datetime.now(timezone.utc) + timedelta(seconds=5))
         hello.delay()
         return HttpResponse('Hello!')
 
--- project/simpleapp/urls.py
+++ project/simpleapp/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from .views import (
     ProductsList, ProductDetail, ProductCreate, ProductUpdate, ProductDelete
 )
+from django.views.decorators.cache import cache_page
+
+
 
 urlpatterns = [
     path('products/', ProductsList.as_view(), name='product_list'),
-    path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
+    path('<int:pk>',cache_page(60*10) (ProductDetail.as_view()), name='product_detail'),
     path('create/', ProductCreate.as_view(), name='product_create'),
     path('<int:pk>/update/', ProductUpdate.as_view(), name='product_update'),
     path('<int:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
